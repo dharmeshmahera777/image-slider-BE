7ec39790db516b06b67944bea1b1dade36a11d07
Dharmesh Mahera <49398768+pratikpatel62@users.noreply.github.com>
2022-05-03 11:51:56 +0530

    remove upload from git

--- services/images.service.js
+++ services/images.service.js
@@ -12,8 +12,8 @@ const saveImages = async(body) => {
 
 const getAllImages = async() => {
     const images = await ImagesModel.find();
-    images.filter(f => f.image && f.image !== '' && f.image.includes("uploads/")).forEach(m=> m.image = "http://localhost:4000/" + m.image);
-    images.filter(f => f.thumbImage && f.thumbImage !== '' && f.thumbImage.includes("uploads/")).forEach(m=> m.thumbImage = "http://localhost:4000/" + m.thumbImage);
+    images.filter(f => f.image && f.image !== '' && f.image.includes("uploads/")).forEach(m=> m.image = "https://image-slider-api.herokuapp.com/" + m.image);
+    images.filter(f => f.thumbImage && f.thumbImage !== '' && f.thumbImage.includes("uploads/")).forEach(m=> m.thumbImage = "https://image-slider-api.herokuapp.com/" + m.thumbImage);
     return images;
 };
 
